af6e5df635d4401ae4571c25d62dba7165aad50f
surajwable <suraj.wable13@gmail.com>
2023-06-25 23:28:30 -0700

    End of Section 3

---
+++ API/Controllers/-- SQLite.sql
@@ -0,0 +1,12 @@
+-- SQLite
+INSERT INTO Users (Id, UserName)
+VALUES (1,"suraj");
+
+INSERT INTO Users (Id, UserName)
+VALUES (2,"pritam");
+
+INSERT INTO Users (Id, UserName)
+VALUES (3,"akash");
+
+INSERT INTO Users (Id, UserName)
+VALUES (4,"vaibhav");
--- API/Program.cs
+++ API/Program.cs
@@ -7,16 +7,21 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 
+
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddCors(); 
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline. 
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+
 app.MapControllers();
 
 app.Run();
